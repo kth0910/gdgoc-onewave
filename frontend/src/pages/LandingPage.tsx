9cf440836206e6acbd7c45a88267c7ce70ebad13
seojin7022 <seojin7022@gmail.com>
2026-02-06 23:57:39 +0900

    Deploy setting

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton, useAuth } from "@clerk/clerk-react";
 
